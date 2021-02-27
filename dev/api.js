f0c0462a5ab711a89a62a8fdb0850ead8184d7e8
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-02-26 23:41:09 -0500

    finished api endpoints

--- dev/api.js
+++ dev/api.js
@@ -2,17 +2,45 @@ const express = require('express');
 const Blockchain = require('./blockchain');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
+const { v4: uuidv4 } = require('uuid');
+const nodeAddress = uuidv4().split('-').join('');
+
+const dogecoin= new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// gets entire blockchain
 app.get('/blockchain', (req, res) => {
-
+    res.send(dogecoin);
 });
 
+// creates a new transaction
 app.post('/transaction', (req, res) => {
-    res.send("Hello");
+    const blockIndex = dogecoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: `Transaction will be added to block ${blockIndex}.`});
 });
 
-app.get('/my_blockchain', (req, res) => {
-
+// mines a new block
+app.get('/mine', (req, res) => {
+    const lastBlock = dogecoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    const currentBlockData = {
+        transactions: dogecoin.pendingTransactions,
+        index: lastBlock['index'] + 1,
+    };
+    const nonce = dogecoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = dogecoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    
+    dogecoin.createNewTransaction(12.5, "00", nodeAddress);
+    
+    const newBlock = dogecoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: "New block mined successfully.",
+        block: newBlock
+    })
 });
 
 app.listen(port, () => {
